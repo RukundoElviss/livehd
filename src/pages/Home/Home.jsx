5186892be255f41e1d872b0fb80eac1a7ad537e7
RukundoElviss <rukundoelvis99@gmail.com>
2024-12-11 16:53:44 +0300

    add trending movies section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
 import heroImg from '../../assets/hero-banner.png';
+import card_img1 from '../../assets/cards/card1.jpg';
+import card_img2 from '../../assets/cards/card2.jpg';
+import card_img3 from '../../assets/cards/card3.jpg';
+import card_img4 from '../../assets/cards/card4.jpg';
+import card_img5 from '../../assets/cards/card5.jpg';
+import card_img6 from '../../assets/cards/card6.jpg';
+import card_img7 from '../../assets/cards/card7.jpg';
+import card_img8 from '../../assets/cards/card8.jpg';
+import card_img9 from '../../assets/cards/card9.jpg';
+import card_img10 from '../../assets/cards/card10.jpg';
 
 const Home = () => {
   return (
-    <div className="home">
+    <section className="home">
       <Navbar />
       <div className="hero container-fluid">
         <div className="row">
@@ -37,7 +47,85 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </div>
+      <div className="row container" id='trending'>
+        <h1 className='col-md-12'>Trending Movies</h1>
+        <div className="card col-md-3 col-6">
+          <div className="card-img-top">
+            <img src={card_img1} alt="Moana 2 Poster" />
+          </div>
+          <div className="card-img-overlay">
+            <h3 className="movie-title">Moana 2</h3>
+            <p className="lead">
+              After receiving an unexpected call from her wayfinding ancestors, Moana must journey to the far seas of Oceania and into dangerous, long-lost waters for an adventure unlike anything she's ever faced.            </p>
+            <div className="overlay-btns">
+              <a href="" className="btn btn-danger">
+                <i className="fas fa-play"></i> Play
+              </a>
+              <a href="" className="btn btn-dark">
+                <i className="fas fa-info-circle"></i> More Info
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card col-md-3 col-6">
+          <div className="card-img-top">
+            <img src={card_img2} alt="Moana 2 Poster" />
+          </div>
+          <div className="card-img-overlay">
+            <h3 className="movie-title">Wicked</h3>
+            <p className="lead">
+              A misunderstood young woman because of her green skin and a popular girl, become friends at Shiz University. After an encounter with the Wonderful Wizard of Oz, their friendship reaches a crossroads.
+            </p>
+            <div className="overlay-btns">
+              <a href="" className="btn btn-danger">
+                <i className="fas fa-play"></i> Play
+              </a>
+              <a href="" className="btn btn-dark">
+                <i className="fas fa-info-circle"></i> More Info
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card col-md-3 col-6">
+          <div className="card-img-top">
+            <img src={card_img3} alt="Moana 2 Poster" />
+          </div>
+          <div className="card-img-overlay">
+            <h3 className="movie-title">Gladiator 2</h3>
+            <p className="lead">
+              After his home is conquered by the tyrannical emperors who now lead Rome, Lucius is forced to enter the Colosseum and must look to his past to find strength to return the glory of Rome to its people.
+            </p>
+            <div className="overlay-btns">
+              <a href="" className="btn btn-danger">
+                <i className="fas fa-play"></i> Play
+              </a>
+              <a href="" className="btn btn-dark">
+                <i className="fas fa-info-circle"></i> More Info
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card col-md-3 col-6">
+          <div className="card-img-top">
+            <img src={card_img4} alt="Moana 2 Poster" />
+          </div>
+          <div className="card-img-overlay">
+            <h3 className="movie-title">Moana 2</h3>
+            <p className="lead">
+              After Santa Claus is kidnapped, the North Pole's Head of Security must team up with a notorious hacker in a globe-trotting, action-packed mission to save Christmas.
+            </p>
+            <div className="overlay-btns">
+              <a href="" className="btn btn-danger">
+                <i className="fas fa-play"></i> Play
+              </a>
+              <a href="" className="btn btn-dark">
+                <i className="fas fa-info-circle"></i> More Info
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
